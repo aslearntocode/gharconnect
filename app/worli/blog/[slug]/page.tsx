bd4a148584e87b4fa9260fe96779e96b126ca382
aslearntocode <aslearntocode@gmail.com>
2025-06-17 16:38:31 +0530

    Added Blogs

--- app/worli/blog/[slug]/page.tsx
+++ app/worli/blog/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import BlogPostPage, { generateMetadata as sharedGenerateMetadata } from '@/app/components/BlogPostPage';
 
-export const generateMetadata = sharedGenerateMetadata;
+export function generateMetadata(props: { params: { slug: string } }) {
+  return sharedGenerateMetadata({ ...props, society: 'worli' });
+}
 
 export default function WorliBlogPostPage(props: { params: { slug: string } }) {
   return <BlogPostPage {...props} society="worli" />;
