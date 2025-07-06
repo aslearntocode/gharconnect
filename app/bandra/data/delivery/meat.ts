76864202e08e304d7057d61dff12199f3f416269
aslearntocode <aslearntocode@gmail.com>
2025-07-06 12:57:23 +0530

    Added a Delivery Vendor

--- app/bandra/data/delivery/meat.ts
+++ app/bandra/data/delivery/meat.ts
@@ -49,6 +49,17 @@ export const vendors: Vendor[] = [
     }],
     mobile: '+91 98679 72116',
     photo: '/images/vendors/Figo_Fresh_Meat.jpeg'
-  }
+  },
+  {
+    name: 'Bombay Fish Supplier',
+    products: [{
+      name: 'Fish',
+      description: 'Fresh fish - fish available',
+      price: 'Call for price',
+      unit: 'per kg'
+    }],
+    mobile: '+91 70213 58630',
+    photo: ''
+  },
 ];
 
