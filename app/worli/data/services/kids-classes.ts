18eb92a6fb98c15c9d0a6411ff5df1a0c0bbaf62
aslearntocode <aslearntocode@gmail.com>
2025-07-18 10:04:49 +0530

    Removed Kids Vendor from Worli

--- app/worli/data/services/kids-classes.ts
+++ app/worli/data/services/kids-classes.ts
@@ -16,45 +16,6 @@ export interface Vendor {
 
 export const vendors: Vendor[] = [
   {
-    name: 'Worli Art Academy',
-    services: [{
-      name: 'Art Classes',
-      description: 'Creative art classes for kids of all ages',
-      price: '₹1500',
-      unit: 'per month'
-    }],
-    mobile: '+91 98765 43211',
-    photo: '/images/services/art-class.jpg',
-    areaServed: [],
-    buildingServed: []
-  },
-  {
-    name: 'Worli Music School',
-    services: [{
-      name: 'Music Classes',
-      description: 'Fun music lessons for children - piano, guitar, vocals',
-      price: '₹1800',
-      unit: 'per month'
-    }],
-    mobile: '+91 87654 32110',
-    photo: '/images/services/music-class.jpg',
-    areaServed: [],
-    buildingServed: []
-  },
-  {
-    name: 'Worli Sports Academy',
-    services: [{
-      name: 'Sports Coaching',
-      description: 'Sports coaching and fitness for kids - cricket, football, swimming',
-      price: '₹2200',
-      unit: 'per month'
-    }],
-    mobile: '+91 76543 21109',
-    photo: '/images/services/sports-class.jpg',
-    areaServed: [],
-    buildingServed: []
-  },
-  {
     name: 'Rushi Kapadia - State Level Tennis Player',
     services: [{
       name: 'Table Tennis Classes',
